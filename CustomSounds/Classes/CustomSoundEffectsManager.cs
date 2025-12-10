058ad6c10795b1d75bce119c7fd5ad5f43ae3e82
TheBlackParrot <theblackparrot0@gmail.com>
2025-12-10 15:34:26 -0600

    fix mono samples using arrays that are too big

--- CustomSounds/Classes/CustomSoundEffectsManager.cs
+++ CustomSounds/Classes/CustomSoundEffectsManager.cs
@@ -256,7 +256,7 @@ internal static class CustomSoundEffectsManager
                     await Awaitable.EndOfFrameAsync();
                 }
                 
-                float[] samples = new float[asset.LengthSamples * 2];
+                float[] samples = new float[asset.LengthSamples * asset.Channels];
                 asset.LoadAllData(samples);
                 
                 clip = AudioClip.Create(Path.GetFileName(soundPath), asset.LengthSamples, asset.Channels, asset.Frequency, false);
